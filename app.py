83e0a7bd8233bef3da9455a6675310bb4c784776
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-04-28 01:22:29 +0400

    Add fetching of affected countries

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.Stats import Stats, CurrentGlobalStats
+from resources.Stats import Stats, GlobalStatsCount, AffectedCountriesDetailed
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
-api.add_resource(Stats, '/stats')
-api.add_resource(CurrentGlobalStats, '/global-stats')
+api.add_resource(Stats, '/global/stats')
+api.add_resource(GlobalStatsCount, '/global/count')
+api.add_resource(AffectedCountriesDetailed, '/global/affected/stats')
--- resources/Stats.py
+++ resources/Stats.py
@@ -1,18 +1,21 @@
 from flask_restful import Resource
 import requests
 
-covid_stats = requests.get('https://pomber.github.io/covid19/timeseries.json')
+pomber_covid_stats = requests.get('https://pomber.github.io/covid19/timeseries.json')
+
+coronastatistics_url = 'http://api.coronastatistics.live'
+
 
 class Stats(Resource):
     def get(self):
-        return {'status': 'success', 'data': covid_stats.json()}, 200
+        return {'status': 'success', 'data': pomber_covid_stats.json()}, 200
 
 
-class CurrentGlobalStats(Resource):
+class GlobalStatsCount(Resource):
     def get(self):
         date = '';
         confirmed = deaths = recovered = active = 0;
-        data = covid_stats.json()
+        data = pomber_covid_stats.json()
         countries = data.keys() 
         for country in countries:
             current = data[country][len(data[country]) - 1]
@@ -20,8 +23,14 @@ class CurrentGlobalStats(Resource):
             confirmed = confirmed + current['confirmed']
             deaths = deaths + current['deaths']
             recovered = recovered + current['recovered']
-            active = active + current['confirmed'] - current['deaths'] - current['recovered']
+            active = active + (current['confirmed'] - current['deaths'] - current['recovered'])
 
         response = {'date': date, 'confirmed': confirmed, 'deaths': deaths, 'recovered':recovered, 'active':active, 'number_of_countries': len(countries)}
 	
         return {'status': 'success', 'data': response}, 200
+
+class AffectedCountriesDetailed(Resource):
+    def get(self):
+        data = requests.get(coronastatistics_url + '/countries').json()
+        return {'status': 'success', 'data': data}, 200
+
