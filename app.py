fd62c434d18d1ca440617b9683192df95d26a227
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-04-27 13:20:43 +0400

    Add fetching of current global stats

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.Stats import Stats
+from resources.Stats import Stats, CurrentGlobalStats
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
 api.add_resource(Stats, '/stats')
+api.add_resource(CurrentGlobalStats, '/global-stats')
--- resources/Stats.py
+++ resources/Stats.py
@@ -1,6 +1,27 @@
 from flask_restful import Resource
+import requests
 
+covid_stats = requests.get('https://pomber.github.io/covid19/timeseries.json')
 
 class Stats(Resource):
     def get(self):
-        return {'status': 'success', 'data': 'Hello'}, 200
+        return {'status': 'success', 'data': covid_stats.json()}, 200
+
+
+class CurrentGlobalStats(Resource):
+    def get(self):
+        date = '';
+        confirmed = deaths = recovered = active = 0;
+        data = covid_stats.json()
+        countries = data.keys() 
+        for country in countries:
+            current = data[country][len(data[country]) - 1]
+            date = current['date']
+            confirmed = confirmed + current['confirmed']
+            deaths = deaths + current['deaths']
+            recovered = recovered + current['recovered']
+            active = active + current['confirmed'] - current['deaths'] - current['recovered']
+
+        response = {'date': date, 'confirmed': confirmed, 'deaths': deaths, 'recovered':recovered, 'active':active, 'number_of_countries': len(countries)}
+	
+        return {'status': 'success', 'data': response}, 200
